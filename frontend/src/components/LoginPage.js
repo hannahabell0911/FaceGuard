8bc0aff7af90d0d3ceda4a7603878aeef3236792
Sanid1707 <pandeysanid@gmail.com>
2023-11-16 01:50:40 +0000

    Initial Login page skin  made

--- frontend/src/components/LoginPage.js
+++ frontend/src/components/LoginPage.js
@@ -1,50 +1,57 @@
 import React from "react";
-import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
 
 const LoginPage = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Col md={6} className="d-none d-md-block bg-image"></Col>
-        <Col md={6} className="login-section-wrapper">
-          <div className="brand-wrapper">
-            <img src="logo.png" alt="logo" className="logo" />
-          </div>
-          <div className="login-wrapper my-auto">
-            <h1 className="login-title">Log in</h1>
-            <Form>
-              <Form.Group>
-                <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="email@example.com" />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  placeholder="Enter your password"
-                />
-              </Form.Group>
-              <Form.Check
-                label="Remember me"
-                style={{ marginBottom: "10px" }}
-              />
-              <Button variant="primary" type="submit" className="login-btn">
-                Login
-              </Button>
-            </Form>
-            <a href="#!" className="forgot-password-link">
-              Forgot password?
-            </a>
-            <p className="login-wrapper-footer-text">
-              Don't have an account?{" "}
-              <a href="#!" className="text-reset">
-                Register here
-              </a>
-            </p>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+    <div className="login-background">
+      <Container className="min-vh-100 d-flex align-items-center justify-content-center">
+        <Row className="justify-content-center">
+          <Col lg={5} md={7} sm={9} className="login-col">
+            <Card className="login-card">
+              <Card.Body>
+                <div className="text-center mb-4">
+                  <img src="logo.png" alt="logo" className="logo" />
+                </div>
+                <Form>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      required
+                    />
+                  </Form.Group>
+                  <Button variant="primary" type="submit" className="w-100">
+                    Login
+                  </Button>
+                </Form>
+                <div className="text-center mt-3">
+                  <a href="#!" className="forgot-password-link">
+                    Forgot password?
+                  </a>
+                </div>
+                <div className="text-center mt-2">
+                  <span className="login-wrapper-footer-text">
+                    Don't have an account?
+                  </span>
+                  <a href="#!" className="text-reset">
+                    Register here
+                  </a>
+                </div>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
