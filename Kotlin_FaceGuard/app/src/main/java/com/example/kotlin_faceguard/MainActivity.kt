da448d692c37aef57fa1217e386d3005a44fa1f6
sanidhyapandey <pandeysanid@gmail.com>
2023-11-22 19:04:21 +0000

    Created Test Feilds

--- Kotlin_FaceGuard/app/src/main/java/com/example/kotlin_faceguard/MainActivity.kt
+++ Kotlin_FaceGuard/app/src/main/java/com/example/kotlin_faceguard/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.kotlin_faceguard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kotlin_faceguard.ui.theme.Kotlin_FaceGuardTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,13 +47,42 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Login()
 {
+    Column(
+        Modifier
+            .padding(24.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Bottom),
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
+        Icon(painter=painterResource(id = R.drawable.faceguard_icon),
+            null,Modifier.size(80.dp),
+            tint = Color.White)
+
+        TextInput()
+        TextInput()
+    }
 
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextInput()
+{
+    var value by remember { mutableStateOf("") }
+
+    TextField(value = value, onValueChange ={value=it} )
+
 //
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    Kotlin_FaceGuardTheme {
-//        Login()
-//    }
-//}
+
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Kotlin_FaceGuardTheme {
+        Login()
+    }
+}
