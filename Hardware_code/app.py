876c093f4a77b22d538b7fcb78644a075015d739
Tiao <1314tiao@gmail.com>
2023-11-24 15:41:41 +0000

    Optimized beep, reduced PIR sensitivity.

--- Hardware_code/app.py
+++ Hardware_code/app.py
@@ -2,25 +2,31 @@ import RPi.GPIO as GPIO
 import time
 
 PIR_pin = 23
-Buzzer_pin = 24 
+Buzzer_pin = 24
+
+# 
+
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR_pin, GPIO.IN)
 GPIO.setup(Buzzer_pin, GPIO.OUT)
 
-def beep():
-    GPIO.output(Buzzer_pin, True)
-    time.sleep(0.3)
-    GPIO.output(Buzzer_pin, False)
+def beep(repeat):
+    for _ in range(repeat):
+        for pulse in range(60):
+            GPIO.output(Buzzer_pin, True)
+            time.sleep(0.001)
+            GPIO.output(Buzzer_pin, False)
+            time.sleep(0.001)
+        time.sleep(0.02)
 
 def motionDetection():
     while True:
         if GPIO.input(PIR_pin):
             print("Motion detected")
-            beep()
+            beep(4)  
         time.sleep(1)
 
 motionDetection()
-
-
