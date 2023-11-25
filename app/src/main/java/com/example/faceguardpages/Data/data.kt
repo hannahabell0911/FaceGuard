0acb37b0623a1e48d1c7a225ec3af068ef9b0328
hannahabell0911 <96234964+hannahabell0911@users.noreply.github.com>
2023-11-25 12:04:11 +0000

    Set up Retrofit in my Activity (model class & data class)

---
+++ app/src/main/java/com/example/faceguardpages/Data/data.kt
@@ -0,0 +1,9 @@
+package com.example.faceguardpages.Data
+
+class data {
+    data class KnownFace(
+        val id: Int,
+        val name: String,
+        val imageUrl: String
+    )
+}
--- app/src/main/java/com/example/faceguardpages/RecognizedFaces.kt
+++ app/src/main/java/com/example/faceguardpages/RecognizedFaces.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import retrofit2.http.GET
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,7 +22,22 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.faceguardpages.Data.data
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 class RecognizedFaces : ComponentActivity() {
+
+    private val apiService: ApiService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://www.faceguard.live/") // Update with your actual API URL
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(ApiService::class.java)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_faces)
@@ -28,6 +47,18 @@ class RecognizedFaces : ComponentActivity() {
         composeView.setContent {
             FacesScreen()
         }
+        lifecycleScope.launch {
+            try {
+                val knownFaces = apiService.getKnownFaces()
+            } catch (e: Exception) {
+                // Handle errors
+            }
+        }
+    }
+    interface ApiService {
+        @GET("known-faces")
+        suspend fun getKnownFaces(): List<data.KnownFace>
+
     }
     @Composable
     fun FacesScreen() {
